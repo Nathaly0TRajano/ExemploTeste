5be6639b6f135bad0f3b3f33b33c7726dbae432b
Nathaly0TRajano <nathydelima00@gmail.com>
2025-03-20 09:48:17 -0300

    adicionado funcionalidades na controller

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,28 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
     public function index(){
-
+        return Tarefa::all();
     }
 
     public function store(Request $request){
-
+        $tarefa = Tarefa::create($request->all());
+        return response()->json($tarefa,201);
     }
 
-    public function show(){
-        
+    public function show(string $id){
+        return Tarefa::find($id);
     }
 
     // string $id -> pra dizer que aquele dado(id no caso) vai ser recebido como string.
+    // só pra didático, esse tipo de código pra update pode gerar vários erros, então o melhor é fazer ele todo.
     public function update(Request $request, string $id){
-
+        $tarefa = Tarefa::find($id);
+        $tarefa->udpate($request->all());
+        return response()->json($tarefa,200);
     }
 
     public function destroy(string $id){
+        $tarefa = Tarefa::find($id)->delete();
+        return response()->json(null, 204);
 
     }
 
