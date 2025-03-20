a197f47a35aa73d7e0c80b5de2d12a389c19bb93
Nathaly0TRajano <nathydelima00@gmail.com>
2025-03-20 09:20:31 -0300

    Adicionado migration, model e controller da tarefa

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarefaController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show(){
+        
+    }
+
+    // string $id -> pra dizer que aquele dado(id no caso) vai ser recebido como string.
+    public function update(Request $request, string $id){
+
+    }
+
+    public function destroy(string $id){
+
+    }
+
+}
